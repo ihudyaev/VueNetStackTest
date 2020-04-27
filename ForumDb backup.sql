1c342837ee5fc3bb02e406fc4d2c9637cf6a9dce
Ilya Hudy <ihud@ubicon-it.ru>
2020-04-27 09:47:32 +0300

    add ForumDb backup

--- ForumDb backup.sql
+++ ForumDb backup.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.2
 -- Dumped by pg_dump version 12.2
 
--- Started on 2020-04-27 01:09:50
+-- Started on 2020-04-27 09:46:06
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -200,6 +200,118 @@ ALTER TABLE public."User" ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
+-- TOC entry 2877 (class 0 OID 16422)
+-- Dependencies: 207
+-- Data for Name: Category; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Category" (id, title, description) FROM stdin;
+1	Films	All films in one place
+2	Music	Some other music
+3	Sport	Sport Themes
+4	Country Flags	Every flag something 
+\.
+
+
+--
+-- TOC entry 2875 (class 0 OID 16406)
+-- Dependencies: 205
+-- Data for Name: Role; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Role" (id, rolename) FROM stdin;
+1	user
+2	moderator
+3	administrator
+\.
+
+
+--
+-- TOC entry 2879 (class 0 OID 16432)
+-- Dependencies: 209
+-- Data for Name: Topic; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Topic" (id, category_id, title, user_id, description, created_at, updated_at, user_displayname) FROM stdin;
+15	1	Films 1990s	10	Films from 1990s	2020-04-26 01:38:11.389765	2020-04-26 01:38:11.389765	testuser
+14	1	Films from 1980's	11	Let's talk about any cool films from 1980's	2020-04-25 00:49:55.230913	2020-04-25 00:49:55.230913	demouser
+16	1	Films from 2000s	10	Cool films from 2000s	2020-04-26 01:41:43.354803	2020-04-26 01:41:43.354803	Harvy not Vainstein
+\.
+
+
+--
+-- TOC entry 2881 (class 0 OID 16442)
+-- Dependencies: 211
+-- Data for Name: Topic_Reply; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Topic_Reply" (id, topic_id, user_id, description, created_at, updated_at, user_displayname) FROM stdin;
+3	14	10	fsdfsdfsdfsdfdsf	2020-04-25 02:21:20.959742	2020-04-25 02:21:20.959742	fsdfasdfsdfafsd
+4	14	10	sdsafsdfsddsdadsads	2020-04-26 02:45:16.992596	2020-04-26 02:45:16.992596	dsadsadasd
+5	14	11	1351351644846465	2020-04-26 14:52:33.273174	2020-04-26 14:52:33.273174	a
+6	14	11	fsdfsdfdsdfsdsdfds	2020-04-26 15:04:00.193416	2020-04-26 15:04:00.193416	a
+\.
+
+
+--
+-- TOC entry 2872 (class 0 OID 16394)
+-- Dependencies: 202
+-- Data for Name: User; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."User" (id, display_name, email, disabled, role_id, "Password", user_name, "Birthdate", "Name", "Surname") FROM stdin;
+10	anonym	a@a.ru	f	1	G3OW3heRkUtFvJcQfKmUxA5iqgmg3cY7IqhhtaDXdug=	anonym	\N	\N	\N
+12	b	b	f	1	Oz1xLzaO1rIyoWEueNzyg2b2rtS79FwZqT29mvkhc2I=	b	\N	\N	\N
+11	a	a	f	1	FwIo3n8lskVq7GSO+ITPMcG0twrpskyLnrQ2UYpqPnQ=	a	2020-04-01	Vanya	Sidorov
+\.
+
+
+--
+-- TOC entry 2887 (class 0 OID 0)
+-- Dependencies: 206
+-- Name: Category_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Category_id_seq"', 4, true);
+
+
+--
+-- TOC entry 2888 (class 0 OID 0)
+-- Dependencies: 204
+-- Name: Role_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Role_id_seq"', 3, true);
+
+
+--
+-- TOC entry 2889 (class 0 OID 0)
+-- Dependencies: 210
+-- Name: Topic_Reply_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Topic_Reply_id_seq"', 6, true);
+
+
+--
+-- TOC entry 2890 (class 0 OID 0)
+-- Dependencies: 208
+-- Name: Topic_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."Topic_id_seq"', 16, true);
+
+
+--
+-- TOC entry 2891 (class 0 OID 0)
+-- Dependencies: 203
+-- Name: User_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public."User_id_seq"', 12, true);
+
+
+--
 -- TOC entry 2731 (class 2606 OID 16429)
 -- Name: Category Category_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
@@ -355,7 +467,7 @@ ALTER TABLE ONLY public."User"
     ADD CONSTRAINT "User_role" FOREIGN KEY (role_id) REFERENCES public."Role"(id) NOT VALID;
 
 
--- Completed on 2020-04-27 01:09:50
+-- Completed on 2020-04-27 09:46:06
 
 --
 -- PostgreSQL database dump complete
